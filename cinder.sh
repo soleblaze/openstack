881e04b9b05d8f99395edecff90ec1fea2011206
soleblaze <soleblaze@skyshadows.net>
2013-04-13 23:14:34 -0500

    Update cinder.sh

--- cinder.sh
+++ cinder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -e "Input Data Interface: "
+echo -n "Input Data Interface: "
 read dataiface
-localip=$(ip addr show $mgtiface | awk '/inet\ / { print $2 }' | cut -d"/" -f1)
+localip=$(ip addr show $dataiface | awk '/inet\ / { print $2 }' | cut -d"/" -f1)
 
 echo -e "Input Controller IP [$dataiface]: "
 read mgtip
