386c50cacee38297dd678c28827ac1c7436730fa
Soleblaze <soleblaze@skyshadows.net>
2015-04-17 21:55:17 -0500

    added proper neutron auth to nova.conf

--- installation-scripts/controller.sh
+++ installation-scripts/controller.sh
@@ -366,6 +366,12 @@ admin_password = $novauser
 host = $glanceip
 
 [neutron]
+url = http://${mgtip}:9696
+auth_strategy = keystone
+admin_auth_url = http://${mgtip}:35357/v2.0
+admin_tenant_name = service
+admin_username = neutron
+admin_password = ${neutronuser}
 service_metadata_proxy = True
 metadata_proxy_shared_secret = ${sharedsecret}
 EOF
