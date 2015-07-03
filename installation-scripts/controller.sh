0f0aa78df53485714b2cc7a5027698eb774b98f3
Soleblaze <soleblaze@skyshadows.net>
2015-07-03 15:05:44 -0500

    Fixed database population

--- installation-scripts/controller.sh
+++ installation-scripts/controller.sh
@@ -561,7 +561,7 @@ fi
 
 # Populate database
 su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf \
---config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade juno" neutron
+--config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron
 
 # Restart neutron
 service neutron-server restart
